1f1aa35e8f72fe42883b9d612613d10eef647638
Quent21 <quent21@yahoo.fr>
2017-05-10 22:13:37 +0200

    Update Bezier.py
    - Utilisation de la méthode récursive
    - Organisation du code (imports, fonctions, interface, canvas, binds)
    - Suppression des variables inutiles

--- Bezier.py
+++ Bezier.py
@@ -2,87 +2,79 @@ from tkinter import *
 from tkinter.ttk import Combobox
 from math import sqrt,factorial as fact
 
-def f(t,liste):
+def f(liste):
+    if sqrt((liste[0][0]-liste[-1][0])**2+(liste[0][1]-liste[-1][1])**2)<1:
+        return(liste[0][0],liste[0][1])
+    l1=liste.copy()
+    l2=liste.copy()
     n=len(liste)
-    return(sum(liste[i][2]*(t**i)*((1-t)**(n-i-1))*liste[i][0] for i in range(n)),sum(liste[i][2]*(t**i)*((1-t)**(n-i-1))*liste[i][1] for i in range(n)))
+    for i in range(n-1):
+        for j in range(i+1):
+            l1[n+j-i-1]=((l1[n+j-i-2][0]+l1[n+j-i-1][0])/2,(l1[n+j-i-2][1]+l1[n+j-i-1][1])/2)
+            l2[i-j]=((l2[i-j][0]+l2[i-j+1][0])/2,(l2[i-j][1]+l2[i-j+1][1])/2)
+    return(f(l1)+f(l2))
 
-def affiche(liste,pas,coeff):
-    p=f(0,liste)
-    points=p[0]*coeff,p[1]*coeff
-    t=pas
-    while t<=1:
-        p2=f(t,liste)
-        p=p2
-        points+=p[0]*coeff,p[1]*coeff
-        t+=pas
-    can.coords(5,points)
+def affiche(liste):
+    points=f(liste)
+    can.coords(1,points)
 
 def clique(event):
     x=event.x
     y=event.y
-    for i in liste:
-        if sqrt((i[0]*100-x)**2+(i[1]*100-y)**2)<=10:
+    for i in range(len(liste)):
+        if sqrt((liste[i][0]-x)**2+(liste[i][1]-y)**2)<=10:
             global select
-            select=indic[liste.index(i)]
+            select=i
             break
+
 def relache(event):
     global select
     select=None
-    
+
 def deplacement(event):
     global select
     if select==None:return
     x=event.x
     y=event.y
     lab.config(text="x,y="+str(x)+" , "+str(y)+" "+str(select))
-    liste[indic.index(select)]=(x/100,y/100,liste[indic.index(select)][2])
-    can.coords(select,x-10,y-10,x+10,y+10)
-    affiche(liste,0.01,100)
+    liste[select]=(x,y)
+    can.coords(select+2,x-10,y-10,x+10,y+10)
+    affiche(liste)
 
-    
 def nvpts():
-    indic.append(can.create_oval(240,240,260,260))
-    can.itemconfig(indic[-1],fill="yellow",outline="red",width=2)
-    liste.append((2.50,2.50))
+    can.create_oval(240,240,260,260,fill="yellow",outline="red",width=2)
+    liste.append((250,250))
     for i in range(len(liste)):
-        liste[i]=(liste[i][0],liste[i][1],fact(len(liste)-1)//(fact(i)*fact(len(liste)-i-1)))
-    affiche(liste,0.01,100)    
-    
+        liste[i]=(liste[i][0],liste[i][1])
+    affiche(liste)
 
-fen=Tk()
-can=Canvas(height=500,width=500,bg="white")
-can.pack(side=LEFT)
-sf=Frame(fen)
-sf.pack(side=RIGHT)
-lab=Label(sf,text="")
-lab.pack()
-fen.config(bg="bisque")
-
-lstbtn=[]
-lstbtn.append(Button(sf,text="Nouveau point",command=nvpts))
-lstbtn[0].pack()
-
+H=500
+W=500
 rayon=10
 
-liste=[(0,0),(0,5),(3,0),(5,5)]
-indic=[1,2,3,4]
-liste=[(liste[i][0],liste[i][1],fact(len(liste)-1)//(fact(i)*fact(len(liste)-i-1))) for i in range(len(liste))]
+liste=[(10,10),(10,H-10),(W-10,10),(W-10,H-10)]
 
-coeff=100
-pas=t=0.01
-p=f(0,liste)
+fen=Tk()
+fen.config(bg="bisque")
+fen.title("Courbe de bézier")
 
-for i in liste:
-    can.create_oval(i[0]*coeff-rayon,i[1]*coeff-rayon,i[0]*coeff+rayon,i[1]*coeff+rayon)
-    can.itemconfig(liste.index(i)+1,fill="yellow",outline="red",width=2)
-points=p[0]*100,p[1]*100
-while t<=1:
-        p2=f(t,liste)
-        p=p2
-        points+=p[0]*100,p[1]*100
-        t+=pas
+sf=Frame(fen)
+sf.grid(row=0,column=1)
+
+lab=Label(sf,text="x,y=...")
+lab.pack()
+
+boutton=Button(sf,text="Nouveau point",command=nvpts)
+boutton.pack()
+
+can=Canvas(height=H,width=W,bg="white")
+can.grid(row=0,column=0)
+
+points=f(liste)
 can.create_line(points,fill="blue",width=2)
 
+for i in range(len(liste)):
+    can.create_oval(liste[i][0]-rayon,liste[i][1]-rayon,liste[i][0]+rayon,liste[i][1]+rayon,fill="yellow",outline="red",width=2)
 
 can.bind("<ButtonPress-1>",clique)
 can.bind("<ButtonRelease-1>",relache)
