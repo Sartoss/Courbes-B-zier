d522f2b5916b3bb1e65085d50ca03457884289a1
Quent21 <quent21@yahoo.fr>
2017-05-20 12:58:33 +0200

    Update Bezier.py

--- Bezier.py
+++ Bezier.py
@@ -20,12 +20,12 @@ def fSpline(liste):
     if len(liste)==2:
         return(liste[0][0],liste[0][1],liste[1][0],liste[1][1])
     points=[]
-    if varBoucle.get()==1:
-        for i in range(-2,len(liste)-2):
-            tax=liste[i+1][0]-liste[i-1][0]
-            tay=liste[i+1][1]-liste[i-1][1]
-            tbx=liste[i+2][0]-liste[i][0]
-            tby=liste[i+2][1]-liste[i][1]
+    if varTangente.get()==1 and CurvType.get()==1:
+        for i in range(-varBoucle.get(),len(liste)-1):
+            tax=liste[i][4]
+            tay=liste[i][5]
+            tbx=-liste[i+1][2]
+            tby=-liste[i+1][3]
             a1=2*liste[i][0]-2*liste[i+1][0]+tax+tbx
             b1=-3*liste[i][0]+3*liste[i+1][0]-2*tax-tbx
             a2=2*liste[i][1]-2*liste[i+1][1]+tay+tby
@@ -36,32 +36,8 @@ def fSpline(liste):
                 points.append(a2*t**3+b2*t**2+tay*t+liste[i][1])
                 t+=0.01
     else:
-        for i in range(len(liste)-1):
-            if i==0:
-                tx=liste[i+2][0]-liste[i][0]
-                ty=liste[i+2][1]-liste[i][1]
-                a1=liste[i][0]/2-liste[i+1][0]/2+tx/2
-                c1=-3*liste[i][0]/2+3*liste[i+1][0]/2-tx/2
-                a2=liste[i][1]/2-liste[i+1][1]/2+ty/2
-                c2=-3*liste[i][1]/2+3*liste[i+1][1]/2-ty/2
-                t=0
-                while t<1:
-                    points.append(a1*t**3+c1*t+liste[i][0])
-                    points.append(a2*t**3+c2*t+liste[i][1])
-                    t+=0.01
-            elif i==len(liste)-2:
-                tx=liste[i+1][0]-liste[i-1][0]
-                ty=liste[i+1][1]-liste[i-1][1]
-                a1=liste[i][0]/2-liste[i+1][0]/2+tx/2
-                b1=-3*liste[i][0]/2+3*liste[i+1][0]/2-3*tx/2
-                a2=liste[i][1]/2-liste[i+1][1]/2+ty/2
-                b2=-3*liste[i][1]/2+3*liste[i+1][1]/2-3*ty/2
-                t=0
-                while t<1:
-                    points.append(a1*t**3+b1*t**2+tx*t+liste[i][0])
-                    points.append(a2*t**3+b2*t**2+ty*t+liste[i][1])
-                    t+=0.01
-            else:
+        if varBoucle.get()==1:
+            for i in range(-2,len(liste)-2):
                 tax=liste[i+1][0]-liste[i-1][0]
                 tay=liste[i+1][1]-liste[i-1][1]
                 tbx=liste[i+2][0]-liste[i][0]
@@ -75,8 +51,48 @@ def fSpline(liste):
                     points.append(a1*t**3+b1*t**2+tax*t+liste[i][0])
                     points.append(a2*t**3+b2*t**2+tay*t+liste[i][1])
                     t+=0.01
-        points.append(liste[-1][0])
-        points.append(liste[-1][1])
+        else:
+            for i in range(len(liste)-1):
+                if i==0:
+                    tx=liste[i+2][0]-liste[i][0]
+                    ty=liste[i+2][1]-liste[i][1]
+                    a1=liste[i][0]/2-liste[i+1][0]/2+tx/2
+                    c1=-3*liste[i][0]/2+3*liste[i+1][0]/2-tx/2
+                    a2=liste[i][1]/2-liste[i+1][1]/2+ty/2
+                    c2=-3*liste[i][1]/2+3*liste[i+1][1]/2-ty/2
+                    t=0
+                    while t<1:
+                        points.append(a1*t**3+c1*t+liste[i][0])
+                        points.append(a2*t**3+c2*t+liste[i][1])
+                        t+=0.01
+                elif i==len(liste)-2:
+                    tx=liste[i+1][0]-liste[i-1][0]
+                    ty=liste[i+1][1]-liste[i-1][1]
+                    a1=liste[i][0]/2-liste[i+1][0]/2+tx/2
+                    b1=-3*liste[i][0]/2+3*liste[i+1][0]/2-3*tx/2
+                    a2=liste[i][1]/2-liste[i+1][1]/2+ty/2
+                    b2=-3*liste[i][1]/2+3*liste[i+1][1]/2-3*ty/2
+                    t=0
+                    while t<1:
+                        points.append(a1*t**3+b1*t**2+tx*t+liste[i][0])
+                        points.append(a2*t**3+b2*t**2+ty*t+liste[i][1])
+                        t+=0.01
+                else:
+                    tax=liste[i+1][0]-liste[i-1][0]
+                    tay=liste[i+1][1]-liste[i-1][1]
+                    tbx=liste[i+2][0]-liste[i][0]
+                    tby=liste[i+2][1]-liste[i][1]
+                    a1=2*liste[i][0]-2*liste[i+1][0]+tax+tbx
+                    b1=-3*liste[i][0]+3*liste[i+1][0]-2*tax-tbx
+                    a2=2*liste[i][1]-2*liste[i+1][1]+tay+tby
+                    b2=-3*liste[i][1]+3*liste[i+1][1]-2*tay-tby
+                    t=0
+                    while t<1:
+                        points.append(a1*t**3+b1*t**2+tax*t+liste[i][0])
+                        points.append(a2*t**3+b2*t**2+tay*t+liste[i][1])
+                        t+=0.01
+            points.append(liste[-1][0])
+            points.append(liste[-1][1])
     return(tuple(points))
 
 def affiche(liste):
